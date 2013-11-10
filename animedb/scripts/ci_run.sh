0185f3b7aa34b168a96f2702b9b57bbc9c6c05d9
miliardopiscrat <miliardopiscrat@gmail.com>
2013-11-10 23:38:06 +0100

    correct build

--- animedb/scripts/ci_run.sh
+++ animedb/scripts/ci_run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-current_folder=$(pwd)
 
-cd ..
 
-export PATH=`pwd`/compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu/bin:$PATH
+export PATH=`pwd`/../compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu/bin:$PATH
 
-cd $current_folder/animedb
+ls $(pwd)/../compiler
+
+cd animedb
 
 make TARGET=linux clean testlib
 
