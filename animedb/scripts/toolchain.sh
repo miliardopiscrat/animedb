a3b823d42fc73bf0eff1dff31c74e1766c1a25eb
miliardopiscrat <miliardopiscrat@gmail.com>
2013-11-11 00:23:17 +0100

    fix toolchain log

--- animedb/scripts/toolchain.sh
+++ animedb/scripts/toolchain.sh
@@ -5,7 +5,7 @@ cd ..
 
 if [ ! -d "compiler" ]; then
 mkdir compiler
-wget http://download.xtreamer.net/Firmware/Prodigy/Toolchain_for_Xtreamer_Jukebox.zip
+wget --quiet http://download.xtreamer.net/Firmware/Prodigy/Toolchain_for_Xtreamer_Jukebox.zip
 unzip Toolchain_for_Xtreamer_Jukebox.zip -d compiler/
 tar -xjf compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu.tar.bz2 -C compiler/
 rm -rf Toolchain_for_Xtreamer_Jukebox.zip
