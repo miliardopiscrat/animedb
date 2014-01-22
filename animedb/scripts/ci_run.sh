eef2d0d9b054b5d7056b54f8bcf2345e225630f9
miliardopiscrat <miliardopiscrat@gmail.com>
2014-01-22 20:05:52 +0100

    add error return in ci script for pipelines

--- animedb/scripts/ci_run.sh
+++ animedb/scripts/ci_run.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-
+set -o pipefail
 
 export PATH=`pwd`/../compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu/bin:$PATH
 
@@ -15,3 +15,11 @@ make TARGET=linux testlib -j4
 make TARGET=linux clean
 make TARGET=mipsel all -j4
 make TARGET=mipsel tarball
+
+
+/animedb/scripts/toolchain.sh
+
+./animedb/scripts/ci_run.sh
+
+cd animedb
+mv animedb.tar.gz animedb-`echo $DRONE_BUILD_NUMBER`.tar.gz
