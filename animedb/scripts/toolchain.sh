36c47b29078c60a0cf555e36ce35a4f6d612339c
miliardopiscrat <miliardopiscrat@gmail.com>
2018-09-02 00:36:52 +0200

    Update toolchain.sh

--- animedb/scripts/toolchain.sh
+++ animedb/scripts/toolchain.sh
@@ -3,7 +3,6 @@ cd ..
 rm -rf compiler
 
 if [ ! -d "compiler" ]; then
-wget -O compiler.tar.gz "https://drive.google.com/uc?export=download&confirm=KctX&id=1fL4TMGhfmXRQ3cOEGa3H3afExt6pMrzR"
 tar -xvzf gdrive.tar.gz
 ./gdrive download 1fL4TMGhfmXRQ3cOEGa3H3afExt6pMrzR
 tar -xvzf compiler.tar.gz
