8ff42614b0695b26b5b77077428fd4cc4c5ef108
miliardopiscrat <miliardopiscrat@gmail.com>
2018-09-01 23:58:44 +0200

    Update toolchain.sh

--- animedb/scripts/toolchain.sh
+++ animedb/scripts/toolchain.sh
@@ -6,8 +6,8 @@ cd ..
 if [ ! -d "compiler" ]; then
 mkdir compiler
 wget --quiet https://drive.google.com/uc?export=download&confirm=KctX&id=1fL4TMGhfmXRQ3cOEGa3H3afExt6pMrzR
-ls compiler
-ls scripts
-tar -xvzf compiler.tar.gz
-rm -rf compiler.tar.gz
+ls ./compiler
+ls ./scripts
+#tar -xvzf compiler.tar.gz
+#rm -rf compiler.tar.gz
 fi
