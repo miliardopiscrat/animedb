39a89e6265607faa0e3b8af1344f54a6b9995d4a
miliardopiscrat <miliardopiscrat@gmail.com>
2018-09-02 00:38:44 +0200

    Update toolchain.sh

--- animedb/scripts/toolchain.sh
+++ animedb/scripts/toolchain.sh
@@ -3,8 +3,9 @@ cd ..
 rm -rf compiler
 
 if [ ! -d "compiler" ]; then
+cd ./scripts
 tar -xvzf gdrive.tar.gz
 ./gdrive download 1fL4TMGhfmXRQ3cOEGa3H3afExt6pMrzR
-tar -xvzf compiler.tar.gz
+tar -xvzf compiler.tar.gz ../
 rm -rf compiler.tar.gz
 fi
