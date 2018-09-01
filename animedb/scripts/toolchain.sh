1fc16d97e6004459800b3efb1794c579b5df0f93
miliardopiscrat <miliardopiscrat@gmail.com>
2018-09-01 23:35:13 +0200

    Update toolchain.sh

--- animedb/scripts/toolchain.sh
+++ animedb/scripts/toolchain.sh
@@ -5,9 +5,7 @@ cd ..
 
 if [ ! -d "compiler" ]; then
 mkdir compiler
-wget --quiet http://download.xtreamer.net/Firmware/Prodigy/Toolchain_for_Xtreamer_Jukebox.zip
-unzip Toolchain_for_Xtreamer_Jukebox.zip -d compiler/
-tar -xjf compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu.tar.bz2 -C compiler/
-rm -rf Toolchain_for_Xtreamer_Jukebox.zip
-rm -rf compiler/*.bz2
+wget --quiet https://drive.google.com/uc?export=download&confirm=KctX&id=1fL4TMGhfmXRQ3cOEGa3H3afExt6pMrzR
+tar -xvzf compiler.tar.gz
+rm -rf compiler.tar.gz
 fi
