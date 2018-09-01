d15f3e1a3ccb154398d0640cd658e37fc99a370a
miliardopiscrat <miliardopiscrat@gmail.com>
2018-09-02 01:45:11 +0200

    Update ci_run.sh

--- animedb/scripts/ci_run.sh
+++ animedb/scripts/ci_run.sh
@@ -4,6 +4,8 @@ export PATH=`pwd`/../compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu/bin:$PATH
 
 cd ..
 
+echo `ls /home/circleci/animedb/animedb/scripts/../compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu/bin/`
+
 ./make TARGET=linux testlib -j4
 
 ./make TARGET=mipsel all -j4
